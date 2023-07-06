98955a5bb57bbe70e97b02b6429958af7087aec1
Angela <angela.fan@oracle.com>
2023-07-06 15:34:44 -0700

    completed soda machine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,6 +39,37 @@ public class Main {
         cupcake.setPrice(price);
         cupcakeMenu.add(chocolate);
 
+
+        ArrayList<Drink> drinkMenu = new ArrayList<Drink>();
+//        Drink water = new Drink();
+//        Soda soda = new Soda();
+//        Milk milk = new Milk();
+
+        System.out.println("We are in the middle of creating the drink menu. We currently have three drinks on the menu but we need to decide on pricing.");
+        input = new Scanner(System.in);
+
+        String[] drinkTypes = {"water", "soda", "milk"};
+
+        for (String drinkType : drinkTypes) {
+            Drink drink = getDrinkByType(drinkType);
+            System.out.println("We are deciding on the price for our " + drinkType + " drink. Here is the description:");
+            drink.type();
+            System.out.println("How much would you like to charge for the drink? (Input a numerical number taken to 2 decimal places)");
+            priceText = input.nextLine();
+            drink.setPrice(price);
+            drinkMenu.add(drink);
+        }
+    }
+
+    private static Drink getDrinkByType(String drinkType) {
+        if (drinkType.equals("water")) {
+            return new Drink();
+        } else if (drinkType.equals("soda")) {
+            return new Soda();
+        } else if (drinkType.equals("milk")) {
+            return new Milk();
+        }
+        throw new IllegalArgumentException("Invalid drink type: " + drinkType);
     }
 }
 
@@ -60,13 +91,46 @@ class Cupcake {
 }
 
 class RedVelvet extends Cupcake {
+    @Override
     public void type() {
         System.out.println("A red velvet based cupcake, with cream cheese frosting.");
     }
 }
 
 class Chocolate extends Cupcake {
+    @Override
     public void type() {
         System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
+}
+
+class Drink {
+    public double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void type() {
+        System.out.println("This is a bottle of water.");
+    }
+
+}
+
+class Soda extends Drink {
+    @Override
+    public void type() {
+        System.out.println("A bottle of soda");
+    }
+}
+
+class Milk extends Drink {
+    @Override
+    public void type() {
+        System.out.println("A bottle of milk");
     }
 }
